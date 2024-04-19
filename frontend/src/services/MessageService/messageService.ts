8e5427d00590f0a4b7d9aecd96dff04694636e3a
Tamires Caires <tamiresdec@gmail.com>
2024-04-19 16:36:59 -0300

    chore

--- frontend/src/services/MessageService/messageService.ts
+++ frontend/src/services/MessageService/messageService.ts
@@ -14,7 +14,6 @@ const handleRequest = async (config: AxiosRequestConfig) => {
     throw error;
   }
 };
-
 export const messageService = {
   sendMessage: async (chatData: CreateChatMessage) => {
     const config: AxiosRequestConfig = {
